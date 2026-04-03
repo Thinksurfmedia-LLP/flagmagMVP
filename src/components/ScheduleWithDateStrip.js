359612b87699eb88269d22fb1e703fce2ec4fe4a
Rishikalpa Das <rishikalpa@thinksurfmedia.info>
2026-04-03 19:37:46 +0530

    made the whole bubble clickable

--- src/components/ScheduleWithDateStrip.js
+++ src/components/ScheduleWithDateStrip.js
@@ -5,42 +5,41 @@ import Link from "next/link";
 
 function MatchCard({ game, orgSlug, seasonSlug }) {
     const gameStatsUrl = `/organizations/${orgSlug}/season/${seasonSlug}/game/${game._id}/stats`;
+    const teamLogoFallback = "/assets/images/team-placeholder.svg";
     return (
         <div className="col-xl-6">
-            <div className="organization-team-area">
-                <div className="top">
-                    <ul>
-                        <li><img src="/assets/images/icon-clock.png" alt="" /> Time - <span>{game.time}</span></li>
-                        <li><img src="/assets/images/icon-calander.png" alt="" /> Date - <span>{new Date(game.date).toLocaleDateString("en-US", { weekday: "short", month: "short", day: "2-digit" })}</span></li>
-                    </ul>
-                </div>
-                <div className="middle">
-                    <div className="a">
-                        <Link href={gameStatsUrl}>
-                            <img src={game.teamA.logo || "/assets/images/team1.png"} alt="" />
-                        </Link>
-                        <h6>{game.teamA.name}</h6>
+            <Link href={gameStatsUrl} style={{ textDecoration: "none", display: "block" }}>
+                <div className="organization-team-area">
+                    <div className="top">
+                        <ul>
+                            <li><img src="/assets/images/icon-clock.png" alt="" /> Time - <span>{game.time}</span></li>
+                            <li><img src="/assets/images/icon-calander.png" alt="" /> Date - <span>{new Date(game.date).toLocaleDateString("en-US", { weekday: "short", month: "short", day: "2-digit" })}</span></li>
+                        </ul>
                     </div>
-                    <div className="b">
-                        {game.status === "completed" ? (
-                            <span>{game.teamA.score} - {game.teamB.score}</span>
-                        ) : (
-                            <span>YET TO BE PLAYED</span>
-                        )}
+                    <div className="middle">
+                        <div className="a">
+                            <img src={game.teamA.logo || teamLogoFallback} alt="" />
+                            <h6>{game.teamA.name}</h6>
+                        </div>
+                        <div className="b">
+                            {game.status === "completed" ? (
+                                <span>{game.teamA.score} - {game.teamB.score}</span>
+                            ) : (
+                                <span>YET TO BE PLAYED</span>
+                            )}
+                        </div>
+                        <div className="c">
+                            <img src={game.teamB.logo || teamLogoFallback} alt="" />
+                            <h6>{game.teamB.name}</h6>
+                        </div>
                     </div>
-                    <div className="c">
-                        <Link href={gameStatsUrl}>
-                            <img src={game.teamB.logo || "/assets/images/team2.png"} alt="" />
-                        </Link>
-                        <h6>{game.teamB.name}</h6>
+                    <div className="bottom">
+                        <ul>
+                            <li><img src="/assets/images/icon-map.png" alt="" /> Locations - <span>{game.location}</span></li>
+                        </ul>
                     </div>
                 </div>
-                <div className="bottom">
-                    <ul>
-                        <li><img src="/assets/images/icon-map.png" alt="" /> Locations - <span>{game.location}</span></li>
-                    </ul>
-                </div>
-            </div>
+            </Link>
         </div>
     );
 }
